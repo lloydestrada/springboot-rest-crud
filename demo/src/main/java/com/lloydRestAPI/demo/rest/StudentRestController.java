61704be7a13a011dcd22a31c87e72a9be38fb310
lloydestrada <iamlloydestrada@gmail.com>
2025-07-16 23:10:20 +0800

    refactored to global exception handling

--- demo/src/main/java/com/lloydRestAPI/demo/rest/StudentRestController.java
+++ demo/src/main/java/com/lloydRestAPI/demo/rest/StudentRestController.java
@@ -47,34 +47,4 @@ public class StudentRestController {
         //just index into the list
         return theStudents.get(studentId);
     }
-
-    //add an exception handler using annotations (@ExceptionHandler)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-
-        //create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        //return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // add another exception handler (catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-
-        //create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage("ERROR");
-        error.setTimeStamp(System.currentTimeMillis());
-
-        //return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
