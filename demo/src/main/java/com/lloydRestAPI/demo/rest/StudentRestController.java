202056a9e976154eeb038550a6f4243595ea62c8
lloydestrada <iamlloydestrada@gmail.com>
2025-07-16 22:11:05 +0800

    refactor code

--- demo/src/main/java/com/lloydRestAPI/demo/rest/StudentRestController.java
+++ demo/src/main/java/com/lloydRestAPI/demo/rest/StudentRestController.java
@@ -2,6 +2,7 @@ package com.lloydRestAPI.demo.rest;
 
 
 import com.lloydRestAPI.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +14,22 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    private List<Student> theStudents;
 
-    //define endpoint for '/students' - return list of students
-    @GetMapping("/students")
-    public List<Student> getStudents(){
+    //define @PostConstruct to load the student data... only once
+    @PostConstruct
+    public void loadData() {
 
-        List<Student> theStudents = new ArrayList<>();
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Lloyd", "Estrada"));
         theStudents.add(new Student("John", "Doe"));
         theStudents.add(new Student("Spongebob", "Squarepants"));
+    }
+
+    //define endpoint for '/students' - return list of students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
 
         return theStudents;
     }
